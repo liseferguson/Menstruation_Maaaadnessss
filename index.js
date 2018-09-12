e86521eb0d69641df745355da08760d4861670be
Lise Ferguson <lise.ferguson503@gmail.com>
2018-09-12 16:51:00 -0700

    added styled to list items

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 'use strict';
-
+//click to make children appear
 $(document).ready(function(){
  $('.container').click(function(event){
-     $(event.target).children('.nested-list').toggle();
+   $(event.target).children('.nested-list').toggle();
  })
 })
+//make children clickable
