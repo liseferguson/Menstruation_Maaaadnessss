b9ab4e7fd8cd318e985405914289e051c7c1cb35
Lise Ferguson <lise.ferguson503@gmail.com>
2018-09-12 18:09:59 -0700

    made some functions

--- index.js
+++ index.js
@@ -8,8 +8,127 @@ $(document).ready(function(){
 })
 //make list items clickable
 
+let currentHateLink = '';
 
-//random link
+function hateEveryone()() {
+  const hateLinks = [];
+  let randomHateLink = hateLinks[Math.floor(Math.random() * hateLinks.length)];
+  currentHateLink = randomHateLink;
+  window.open(randomHateLink, '_blank');
+  return randomHateLink;
+}
+
+let currentLoveLink = '';
+
+function loveStinks() {
+  const loveLinks = [];
+  let randomLoveLink = loveLinks[Math.floor(Math.random() * loveLinks.length)];
+  currentLoveLink = randomLoveLink;
+  window.open(randomLoveLink, '_blank');
+  return randomLoveLink;
+}
+
+let currentPatriarchLink = '';
+
+function patriarchSmash() {
+  const patriarchLinks = [
+    "http://feministryangosling.tumblr.com/",
+    "http://wearewhatfeministslooklike.tumblr.com/",
+    "http://ladiesagainsthumanity.tumblr.com/",
+  ];
+  let randomPatriarchLink = patriarchLinks[Math.floor(Math.random() * patriarchLinks.length)];
+  currentPatriarchLink = randomPatriarchLink;
+  window.open(randomPatriarchLink, '_blank');
+  return randomPatriarchLink;
+}
+
+let currentMakeHappyLink = '';
+
+function makeHappy() {
+  const makeHappyLinks = [];
+  let randomMakeHappyLink = makeHappyLinks[Math.floor(Math.random() * makeHappyLinks.length)];
+  currentMakeHappyLink = randomMakeHappyLink;
+  window.open(randomMakeHappyLink, '_blank');
+  return randomMakeHappyLink;
+}
+
+let currentNeverHappyLink = '';
+
+function neverHappy() {
+  const neverHappyLinks = [];
+  let randomNeverHappyLink = neverHappyLinks[Math.floor(Math.random() * neverHappyLinks.length)];
+  currentNeverHappyLink = randomNeverHappyLink;
+  window.open(randomNeverHHappyLink, '_blank');
+  return randomNeverHappyLink;
+}
+
+let currentSmutLink = '';
+
+function smutMe() {
+  const smutLinks = [];
+  let randomSmutLink = smutLinks[Math.floor(Math.random() * smutLinks.length)];
+  currentSmutLink = randomSmutLink;
+  window.open(randomSmutLink, '_blank');
+  return randomSmutLink;
+}
+
+//chnage function below to match HTML function names 
+
+let currentPatriarchLink = '';
+
+function patriarchSmash() {
+  const patriarchLinks = [
+    "http://feministryangosling.tumblr.com/",
+    "http://wearewhatfeministslooklike.tumblr.com/",
+    "http://ladiesagainsthumanity.tumblr.com/",
+  ];
+  let randomPatriarchLink = patriarchLinks[Math.floor(Math.random() * patriarchLinks.length)];
+  currentPatriarchLink = randomPatriarchLink;
+  window.open(randomPatriarchLink, '_blank');
+  return randomPatriarchLink;
+}
+
+let currentPatriarchLink = '';
+
+function patriarchSmash() {
+  const patriarchLinks = [
+    "http://feministryangosling.tumblr.com/",
+    "http://wearewhatfeministslooklike.tumblr.com/",
+    "http://ladiesagainsthumanity.tumblr.com/",
+  ];
+  let randomPatriarchLink = patriarchLinks[Math.floor(Math.random() * patriarchLinks.length)];
+  currentPatriarchLink = randomPatriarchLink;
+  window.open(randomPatriarchLink, '_blank');
+  return randomPatriarchLink;
+}
+
+let currentPatriarchLink = '';
+
+function patriarchSmash() {
+  const patriarchLinks = [
+    "http://feministryangosling.tumblr.com/",
+    "http://wearewhatfeministslooklike.tumblr.com/",
+    "http://ladiesagainsthumanity.tumblr.com/",
+  ];
+  let randomPatriarchLink = patriarchLinks[Math.floor(Math.random() * patriarchLinks.length)];
+  currentPatriarchLink = randomPatriarchLink;
+  window.open(randomPatriarchLink, '_blank');
+  return randomPatriarchLink;
+}
+
+let currentPatriarchLink = '';
+
+function patriarchSmash() {
+  const patriarchLinks = [
+    "http://feministryangosling.tumblr.com/",
+    "http://wearewhatfeministslooklike.tumblr.com/",
+    "http://ladiesagainsthumanity.tumblr.com/",
+  ];
+  let randomPatriarchLink = patriarchLinks[Math.floor(Math.random() * patriarchLinks.length)];
+  currentPatriarchLink = randomPatriarchLink;
+  window.open(randomPatriarchLink, '_blank');
+  return randomPatriarchLink;
+
 let currentPatriarchLink = '';
 
 function patriarchSmash() {
